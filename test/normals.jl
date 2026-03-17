6f758592075b0e288b10ad0022eca0c41de05708
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2026-03-17 13:05:35 +0100

    Test SlicedNormal on all demo data

--- test/normals.jl
+++ test/normals.jl
@@ -1,11 +1,4 @@
-@testset "SlicedNormal" begin
-    δ = readdlm("../demo/data/circle.csv", ',')
-
-    d = 3
-    b = 10000
-
-    sn, _ = SlicedNormal(δ, d, b)
-
+function shared_tests(sn::SlicedDistributions.SlicedNormal, δ::AbstractMatrix{<:Real})
     @test all(insupport.(sn, eachcol(δ)))
 
     @test hcubature(x -> pdf(sn, x), sn.lb, sn.ub)[1] ≈ 1.0 atol = 1e-3
@@ -13,7 +6,64 @@
     samples = rand(sn, 1000)
 
     @test all(insupport.(sn, eachcol(samples)))
+    @test all(pdf.(sn, eachcol(samples)) .>= 0)
+end
 
-    @test repr(sn) ==
-        "SlicedNormal(nδ=2, d=3, nz=9,\n  lb=[-3.2929135595124106, -3.453912509293032],\n  ub=[3.3151850678141344, 3.3768332192657207])"
+@testset "SlicedNormal" begin
+    @testset "Banana" begin
+        δ = readdlm("../demo/data/banana.csv", ',')
+
+        lb = [-4.0, 0.0]
+        ub = [4.0, 65.0]
+
+        d = 2
+        b = 40000
+
+        sn, _ = SlicedNormal(δ, d, b; lb, ub)
+
+        shared_tests(sn, δ)
+
+        @test repr(sn) ==
+            "SlicedNormal(nδ=2, d=2, nz=5,\n  lb=[-4.0, 0.0],\n  ub=[4.0, 65.0])"
+    end
+
+    @testset "Circle" begin
+        δ = readdlm("../demo/data/circle.csv", ',')
+
+        d = 3
+        b = 10000
+
+        lb = [-4.0, -4.0]
+        ub = [4.0, 4.0]
+
+        sn, _ = SlicedNormal(δ, d, b; lb, ub)
+
+        shared_tests(sn, δ)
+
+        @test repr(sn) ==
+            "SlicedNormal(nδ=2, d=3, nz=9,\n  lb=[-4.0, -4.0],\n  ub=[4.0, 4.0])"
+
+        sn, _ = SlicedNormal(δ, d, b)
+
+        shared_tests(sn, δ)
+
+        @test repr(sn) ==
+            "SlicedNormal(nδ=2, d=3, nz=9,\n  lb=[-3.2929135595124106, -3.453912509293032],\n  ub=[3.3151850678141344, 3.3768332192657207])"
+    end
+
+    @testset "Swirl" begin
+        δ = readdlm("../demo/data/swirl.csv", ',')
+
+        lb = [-18, -18]
+        ub = [18, 18]
+
+        d = 7
+        b = 80000
+
+        sn, _ = SlicedNormal(δ, d, b; lb, ub)
+
+        shared_tests(sn, δ)
+
+        @test repr(sn) == "SlicedNormal(nδ=2, d=7, nz=35,\n  lb=[-18, -18],\n  ub=[18, 18])"
+    end
 end
